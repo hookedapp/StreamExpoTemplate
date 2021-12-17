5c3ead224c97754e6e03f5f5d0c6dd5bc5504956
Nikolas Andryuschenko <nikolaigeorgie@gmail.com>
2021-12-16 17:40:10 -0800

    Add env variable logs

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+
+useEffect(() => {
+  console.log("GET MENU!")
+      console.log('Constants.manifest.extra.enableComments', Constants.manifest.extra)
+            console.log('Constants.manifest.extra.enableComments', Constants.manifest.extra.apiUrl)
+                        console.log('Constants.manifest.extra.enableComments', Constants.manifest.extra.locationId)
+                                    console.log('Constants.manifest.extra.enableComments', Constants.manifest.extra.accessToken)
+}, [])
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
